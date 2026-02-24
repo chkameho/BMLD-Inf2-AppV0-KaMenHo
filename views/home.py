6f1211f2ce57a1cc69f5a067b49bedb592766bf7
chkameho <kamen1995@hotmail.com>
2026-02-24 22:24:58 +0100

    updated my name

--- views/home.py
+++ views/home.py
@@ -6,8 +6,7 @@ st.title("Meine erste Streamlit App")
 
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Anna Müller (anmu@zhaw.ch)
-- Peter Meier (peme@zhaw.ch)
+- Ka Men Ho
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
