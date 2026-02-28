070f148a6600f470fe10adaf690b7f5cab86f239
chkameho <kamen1995@hotmail.com>
2026-02-28 14:09:17 +0100

    updated name

--- views/home.py
+++ views/home.py
@@ -6,7 +6,7 @@ st.title("Meine erste Streamlit App")
 
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Ka Men Ho
+- Ka Men Ho (hokam001@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
