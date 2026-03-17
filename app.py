5971b4f9f4eca3c6e7abe1f2f52d99b3d55897b8
chkameho <kamen1995@hotmail.com>
2026-03-17 23:11:18 +0100

    added login manager and save function

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 import streamlit as st
+import pandas as pd
+from utils.data_manager import DataManager
+from utils.login_manager import LoginManager
 
 st.set_page_config(page_title="Meine App", page_icon=":material/home:")
+
+data_manager = DataManager(
+    fs_protocol ="webdav",
+    fs_root_folder = "BMLD_App_DB"
+)
+
+login_manager = LoginManager(data_manager)
+login_manager.login_register()
+
+if "data_df" not in st.session_state:
+    st.session_state["data_df"] = data_manager.load_user_data(
+        "data.csv",
+        initial_value = pd.DataFrame(columns=["multiplication"]),
+        parse_dates = ["timestamp"]
+    )
 
 pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
 pg_second = st.Page("views/unterseite_a.py", title="Unterseite A", icon=":material/info:")
--- views/unterseite_a.py
+++ views/unterseite_a.py
@@ -1,5 +1,24 @@
 import streamlit as st
+from functions.calculator import  multiplication
+import pandas as pd 
+from utils.data_manager import DataManager
 
-st.title("Unterseite A")
+st.title("Multiplication")
 
 st.write("Diese Seite ist eine Unterseite der Startseite.")
+
+st.divider()
+number_a = st.number_input("")
+number_b = st.number_input(" ")
+st.divider()
+
+if st.button("submitt"):
+    result = multiplication(number_a,number_b)
+    st.session_state['data_df'] = pd.concat([st.session_state['data_df'], pd.DataFrame(data = {"multiplication" : [result]})])
+
+        # --- CODE UPDATE: save data to data manager ---
+    data_manager = DataManager()
+    data_manager.save_user_data(st.session_state['data_df'], 'data.csv')
+
+
+st.write(st.session_state["data_df"])
